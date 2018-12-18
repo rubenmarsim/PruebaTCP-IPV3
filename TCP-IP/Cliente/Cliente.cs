d5a52ece460a70469bbfdada9a09d322487c1f88
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2018-12-18 19:21:43 +0100

    power

---
+++ TCP-IP/Cliente/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cliente
+{
+    class Cliente
+    {
+        static void Main(string[] args)
+        {
+            //Creamos el TCPClient asociado el server
+            TcpClient client = new TcpClient("127.0.0.1", 21);
+
+            //Convertimos un mensaje a un array de bytes
+            byte[] dades = Encoding.ASCII.GetBytes("Hola soy el cliente");
+
+            //Instanciamos un NetworkStream para leer y escribir a partir de GetStream
+            NetworkStream NS = client.GetStream();
+
+            //Enviamos el mensaje al server
+            NS.Write(dades, 0, dades.Length);
+
+            //Preparamos el socket para que escuche la respuesta del server
+            var dadaResposta = new byte[256];
+            string resposta = string.Empty;
+            Int32 bytes = NS.Read(dadaResposta, 0, dadaResposta.Length);
+            resposta = Encoding.ASCII.GetString(dadaResposta, 0, bytes);
+            Console.WriteLine(resposta);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TCP-IP/Server/Server.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            //Instanciamos el server
+            TcpListener Listener = new TcpListener(IPAddress.Any, 21);
+            //lo iniciamos
+            Listener.Start();
+
+            //Creamos un socket de tipo TCPClient preparado para enviar y recibir info
+            TcpClient client = Listener.AcceptTcpClient();
+
+            //Creamos el NetworkStream asociado al client
+            NetworkStream NS = client.GetStream();
+            //Dimensionamos el buffer
+            byte[] buffer = new byte[1024];
+
+            //Creamos un nuevo buffer, el cual usaremos para enviar datos,
+            //y lo llenamos
+            byte[] nouBuffer = Encoding.ASCII.GetBytes("Hola soy el server");
+            //le decimos al NetworkStream que printee lo del nuevo buffer, y le pasamos
+            //los demas parametros que necesita
+            NS.Write(nouBuffer, 0, nouBuffer.Length);
+
+            Console.WriteLine(nouBuffer);
+            Console.ReadKey();
+            //Paramos el server
+            NS.Close();
+            client.Close();
+            Listener.Stop();
+        }
+    }
+}
